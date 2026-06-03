4392d5656c80aa047c711a06f0b4b74a2ffb81f6
biancacatarinovelozo <bianca.velozo@escola.pr.gov.br>
2026-06-03 08:53:51 -0300

    s

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
-// Aguarda o carregamento do documento
-document.addEventListener("DOMContentLoaded", () => {
-    
-    // Seleção dos elementos do Menu Hambúrguer
-    const hamburger = document.getElementById("hamburger");
-    const navMenu = document.getElementById("nav-menu");
-    const navLinks = document.querySelectorAll(".nav-link");
+// Seleção dos elementos do Menu Hambúrguer
+const hamburger = document.querySelector("#menuBtn");
+const navMenu = document.querySelector("#navMenu");
+const navLinks = document.querySelectorAll(".nav-link");
 
-    // Função para alternar a abertura/fechamento do menu
-    function toggleMenu() {
-        hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
-    }
+// Função para abrir e fechar o menu ao clicar no botão
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
 
-    // Evento de clique no botão hambúrguer
-    hamburger.addEventListener("click", toggleMenu);
-
-    // Fecha o menu automaticamente quando o usuário clicar em qualquer link (âncora)
-    navLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            if (navMenu.classList.contains("active")) {
-                toggleMenu();
-            }
-        });
+// Evento para fechar o menu automaticamente ao clicar em qualquer tópico/link
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
     });
 });
